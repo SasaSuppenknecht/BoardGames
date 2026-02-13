903a5ed4e51d80c3a593f26f5844fedb98d08cb9
SasaSuppenknecht <sascha.hofmann2000@web.de>
2026-02-13 10:24:39 +0100

    Basic menu complete

--- menu/HostMenu.cs
+++ menu/HostMenu.cs
@@ -1,6 +1,17 @@
 using Godot;
 using System;
 
-public partial class HostMenu : PanelContainer {
+public partial class HostMenu : VBoxContainer {
+
+    [Export] private Button _back;
+    [Export] private Button _start;
     
+    [Export] private MainMenu _mainMenu;
+
+    public override void _Ready() {
+        _back.Pressed += () => {
+            _mainMenu.Show();
+            Hide();
+        };
+    }
 }
